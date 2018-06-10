7524245ec5a053330e069ff724804fc126c11048
bestbrain10 <bestbrain10@gmail.com>
2018-06-11 00:19:27 +0100

    prep for publish

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
 
 
-require('mongoose').connect('mongodb://localhost/moloquent');
-const app = require('express')();
-const bp  = require('body-parser');
-
-app.use(bp.json())
-app.use(bp.urlencoded({extended : true}));
-
-app.use('/posts', require('./routes/Posts'))
-app.use('/users', require('./routes/User'))
-
-let port = 4000;
-app.listen(port, ()=>{
-    console.log('app online on port '+ port);
-})
+module.exports = {
+    Model : require('./Packages/Model')
+}
